22071bbd9996aa7b9174ed760f15cf6c27a2a73d
kyota1210 <kytmbmp1210@gmail.com>
2020-10-06 17:29:01 +0900

    共通presenceオプションをまとめる処理

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,14 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
   NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  
   validates_format_of :password, with: PASSWORD_REGEX
-  validates           :nickname, presence: true
-  validates           :first_name, presence: true, format: {with: NAME_REGEX}
-  validates           :last_name, presence: true, format: {with: NAME_REGEX}
-  validates           :read_first_name, presence: true, format: {with: NAME_KANA_REGEX}
-  validates           :read_last_name, presence: true, format: {with: NAME_KANA_REGEX}
-  validates           :birthday, presence: true
+  with_options presence: true do
+    validates           :nickname
+    validates           :first_name, format: {with: NAME_REGEX}
+    validates           :last_name, format: {with: NAME_REGEX}
+    validates           :read_first_name, format: {with: NAME_KANA_REGEX}
+    validates           :read_last_name, format: {with: NAME_KANA_REGEX}
+    validates           :birthday
+  end
 end
