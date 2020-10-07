1b90c940df111929783a58ae1bbc6657748946bc
kyota1210 <kytmbmp1210@gmail.com>
2020-10-07 17:02:09 +0900

    JavaScriptによる販売価格の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,34 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: [:new]
+
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Itme.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :image, :title, :exlanation, :category_id, :status_id, :transfer_fee_id, 
+      :from_address_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
