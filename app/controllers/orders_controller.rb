f89fe57f913f0c67c1d85380f3196bacb86afe38
kyota1210 <kytmbmp1210@gmail.com>
2020-10-13 11:18:47 +0900

    商品購入機能のユーザー遷移修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   before_action :item_find_params_item_id, only: [:index, :create]
   before_action :move_to_top_page, only: [:index]
-  before_action :authenticate_user!, only: [:index]
 
   def index
     @order_address = OrderAddress.new
